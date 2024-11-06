142c823c461ace89dd39b81925b88159b3880794
Khaled Karim <kskarim177@gmail.com>
2024-11-07 01:04:21 +0600

    Services,Contact Us model created

--- contact_us/models.py
+++ contact_us/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=25)
+    phone = models.CharField(max_length=11)
+    problem = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Contact Us"
+
