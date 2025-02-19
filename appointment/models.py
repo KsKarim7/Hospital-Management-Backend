d72c31c4dafbd2fb69149a281650244f45129fa0
Khaled Karim <kskarim177@gmail.com>
2025-02-19 07:25:01 +0600

    Some modifications

--- appointment/models.py
+++ appointment/models.py
@@ -9,19 +9,18 @@ APPOINTMENT_STATUS = [
     ("Running","Running"),
 ]
 APPOINTMENT_TYPES = [
-    ("Offline","Offline"),
-    ("Online","Online"),
+    ('Offline', 'Offline'),
+    ('Online', 'Online'),
 ]
 
 class Appointment(models.Model):
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    appointment_type = models.CharField(choices=APPOINTMENT_TYPES,max_length=10)
-    appointment_status = models.CharField(choices=APPOINTMENT_STATUS,max_length=10, default="Pending")
+    patient = models.ForeignKey(Patient, on_delete = models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete = models.CASCADE)
+    appointment_types = models.CharField(choices = APPOINTMENT_TYPES, max_length = 10)
+    appointment_status = models.CharField(choices = APPOINTMENT_STATUS, max_length = 10, default = "Pending")
     symptom = models.TextField()
-    time = models.OneToOneField(AvailableTime, on_delete=models.CASCADE)
-    cancel = models.BooleanField(default=False)
-
-
+    time = models.OneToOneField(AvailableTime, on_delete = models.CASCADE)
+    cancel = models.BooleanField(default = False)
+    
     def __str__(self):
-        return f"Doctor: {self.doctor.user.first_name} , Patient: {self.patient.user.first_name}"
+        return f"Doctor : {self.doctor.user.first_name} , Patient : {self.patient.user.first_name}"
